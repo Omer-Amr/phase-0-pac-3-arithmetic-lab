cc39fdf602d6b0908f6ef839cff6509aa2f133ba
omer <you@example.com>
2022-10-28 21:44:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+let  a = Math.floor(Math.random() * 1000)
+let  b = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+    return (a+b);
+    console.log (result);
+}
+function subtract(a, b) {
+    return(a-b);
+    conole.log(result);
+}
+function multiply(a, b) {
+    return(a*b);
+    console.log(result);
+}
+function divide(a,b) {
+    return (a/b);
+    console.log(result);
+}
+function increment(a) {
+    return (a += 1);
+    console.log(result);
+}
+function decrement(a) {
+    return (a -= 1);
+    console.log(result);
+}
+function makeInt(n) {
+    return (parseInt(n, 10));
+}
+function preserveDecimal(n){
+    return(parseFloat(n));
+}
